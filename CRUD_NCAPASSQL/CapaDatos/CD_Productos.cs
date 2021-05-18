24f6d58f95283c50f1585969c2de4e1adb2013d3
Gabriela <gabrielabaez270914@gmail.com>
2021-05-17 23:13:10 -0400

    ProyectoCRUD

---
+++ CRUD_NCAPASSQL/CapaDatos/CD_Productos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    public class CD_Productos
+    {
+        private CD_Conexion conexion = new CD_Conexion();
+
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+        public DataTable Mostrar()
+        {
+            
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "Select * from Productos";
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+                
+        }
+
+        public void Insertar(string nombre, string desc, string marca, double precio, int stock)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "insert into Productos values ('"+nombre+"','"+desc+"','"+marca+"',"+precio+","+stock+")";
+            comando.ExecuteNonQuery();
+        }
+
+        public void Editar(string nombre, string desc, string marca, double precio, int stock, int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            comando.Parameters.AddWithValue("@desc", desc);
+            comando.Parameters.AddWithValue("@marca", marca);
+            comando.Parameters.AddWithValue("@precio", precio);
+            comando.Parameters.AddWithValue("@stock", stock);
+            comando.Parameters.AddWithValue("@id", id);
+        }
+
+        public void Eliminar(int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EliminarProductos";
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@idpro", id);
+
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+        }
+    }
+}
---
+++ CRUD_NCAPASSQL/CapaPresentacion/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaNegocio;
+
+namespace CapaPresentacion
+{
+    public partial class Form1 : Form
+    {
+
+        CN_Productos objetoCN = new CN_Productos();
+        private string idProducto = null;
+        private bool Editar = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MostrarProductos();
+        }
+
+        private void MostrarProductos()
+        {
+            CN_Productos objeto = new CN_Productos();
+            dataGridView1.DataSource = objeto.MostrarProd();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (Editar == false)
+            {
+                try
+                {
+                    objetoCN.InsertarPRod(txtNombre.Text, txtDesc.Text, txtMarca.Text, txtPrecio.Text, txtStock.Text);
+                    MessageBox.Show("Se insertó correctamente.");
+                    MostrarProductos();
+                    limpiarForm();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No se pudo insertar los datos por: " + ex);
+                }
+            }
+
+            if (Editar == true)
+            {
+                try
+                {
+                    objetoCN.EditarProd(txtNombre.Text, txtDesc.Text, txtMarca.Text, txtPrecio.Text, txtStock.Text,idProducto);
+                    MessageBox.Show("Se editó correctamente.");
+                    MostrarProductos();
+                    limpiarForm();
+                    Editar = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No se pudo editar los datos por: " + ex);
+                }
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                Editar = true;
+                txtNombre.Text = dataGridView1.CurrentRow.Cells["Nombre"].Value.ToString();
+                txtDesc.Text = dataGridView1.CurrentRow.Cells["Descripcion"].Value.ToString();
+                txtMarca.Text = dataGridView1.CurrentRow.Cells["Marca"].Value.ToString();
+                txtPrecio.Text = dataGridView1.CurrentRow.Cells["Precio"].Value.ToString();
+                txtStock.Text = dataGridView1.CurrentRow.Cells["Stock"].Value.ToString();
+                idProducto = dataGridView1.CurrentRow.Cells["Id"].Value.ToString();
+            }
+            else
+                MessageBox.Show("Por favor seleccione una fila a editar.");
+        }
+
+        private void limpiarForm()
+        {
+            txtDesc.Clear();
+            txtMarca.Text = "";
+            txtPrecio.Clear();
+            txtStock.Clear();
+            txtNombre.Clear();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                idProducto = dataGridView1.CurrentRow.Cells["Id"].Value.ToString();
+                objetoCN.EliminarProd(idProducto);
+                MessageBox.Show("Se eliminó correctamente.");
+                MostrarProductos();
+            }
+            else
+                MessageBox.Show("Por favor seleccione una fila a eliminar.");
+        }
+    }
+}
